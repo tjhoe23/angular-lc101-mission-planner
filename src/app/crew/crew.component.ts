13c57f8e576843dbb32538ad1461779193a045c1
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-03-13 19:15:55 -0500

    added a way to remember crew

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -21,4 +21,10 @@ export class CrewComponent implements OnInit {
   add(memberName: string, isFirst: boolean) {
     this.crew.push({name: memberName, firstMission: isFirst});
   }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  
 }
